49cf2579f0a19d584e4e36ebdf48da25e06fa175
Rodrigo Bossini <professorbossini@gmail.com>
2021-11-08 11:36:10 -0300

    Exercicios 1 e 2 logo depois do Bloco de Codigo 5.4.1 resolvidos

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ app.use(express.json());
 
 const porta = 3000;
 
+app.get('/pacientes', (req, res) => {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'rodrigo',
+        password: '1234',
+        database: '20212_usjt_ads1ammcb_hospital',
+    })
+    const comandoSQL = 'SELECT * FROM tb_paciente'
+    connection.query(comandoSQL, (erro, resultados, campos) =>{
+        res.json(resultados);
+    })
+})
+
+
 app.get('/medicos', (req, res) => {
     const connection = mysql.createConnection({
         host: 'localhost',
@@ -19,7 +33,7 @@ app.get('/medicos', (req, res) => {
         console.log(erro)
         console.log(resultados)
         console.log(campos)
-        res.end()
+        res.json(resultados)
     })
 })
 
